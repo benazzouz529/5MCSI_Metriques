5b6666acd060e6a0d2d4497ad6fae87ed8a6530e
benazzouz529 <146001300+benazzouz529@users.noreply.github.com>
2024-11-21 15:45:24 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -26,6 +26,10 @@ def meteo():
 def mongraphique():
     return render_template("graphique.html")
   
+@app.route("/histogramme/")
+def histogramme():
+    return render_template("histogramme.html")
+  
 @app.route('/')
 def hello_world():
     return render_template('hello.html')#comments
